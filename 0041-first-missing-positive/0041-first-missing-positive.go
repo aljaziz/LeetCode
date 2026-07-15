827ca653a1e03c2532fa2b0b0a6a403781d8799a
K.M. Al Jaziz Turja <159911068+aljaziz@users.noreply.github.com>
2026-07-15 20:57:49 +0600

    Time: 3 ms (43.25%), Space: 10.2 MB (57.98%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.go
+++ 0041-first-missing-positive/0041-first-missing-positive.go
@@ -1,14 +1,36 @@
 func firstMissingPositive(nums []int) int {
-	hashMap := make(map[int]bool)
-	for _, num := range nums {
-		hashMap[num] = true
+	answerWindow := len(nums)
+
+	foundOne := false
+	for i := 0; i < answerWindow; i++ {
+		if nums[i] == 1 {
+			foundOne = true
+		}
+		if nums[i] <= 0 || nums[i] > answerWindow {
+			nums[i] = 1
+		}
 	}
-	smallPos := 1
-	for i := 0; i < len(nums); i++ {
-        if _, ok := hashMap[smallPos]; !ok {
-            return smallPos
-        }
-        smallPos++
+	if !foundOne {
+		return 1
 	}
-    return smallPos
+
+	for i := 0; i < answerWindow; i++ {
+		val := int(math.Abs(float64(nums[i])))
+		if val == answerWindow {
+			nums[0] = -int(math.Abs(float64(nums[0])))
+		} else {
+			nums[val] = -int(math.Abs(float64(nums[val])))
+		}
+	}
+
+	for i := 1; i < answerWindow; i++ {
+		if nums[i] > 0 {
+			return i
+		}
+	}
+
+	if nums[0] > 0 {
+		return answerWindow
+	}
+	return answerWindow + 1
 }
