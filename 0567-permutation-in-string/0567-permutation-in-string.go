b2abcc6c95cc29d7a1a65d455a9f54fd16bf2567
K.M. Al Jaziz Turja <159911068+aljaziz@users.noreply.github.com>
2026-07-16 16:28:36 +0600

    Time: 0 ms (100%), Space: 4.5 MB (87.61%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.go
+++ 0567-permutation-in-string/0567-permutation-in-string.go
@@ -1,32 +1,22 @@
-import "maps"
-
-func match(s1, s2 map[byte]int) bool {
-	return maps.Equal(s1, s2)
-}
-
 func checkInclusion(s1 string, s2 string) bool {
 	if len(s1) > len(s2) {
 		return false
 	}
 
-	hashMap1 := map[byte]int{}
-	hashMap2 := map[byte]int{}
+	hashMap1 := [26]int{0}
+	hashMap2 := [26]int{0}
 
 	for i := 0; i < len(s1); i++ {
-		hashMap1[s1[i]]++
-		hashMap2[s2[i]]++
+		hashMap1[s1[i]-'a']++
+		hashMap2[s2[i]-'a']++
 	}
 
 	for i := 0; i < len(s2)-len(s1); i++ {
-		if match(hashMap1, hashMap2) {
+		if hashMap1 == hashMap2 {
 			return true
 		}
-		if hashMap2[s2[i]] > 1 {
-			hashMap2[s2[i]]--
-		} else {
-			delete(hashMap2, s2[i])
-		}
-		hashMap2[s2[i+len(s1)]]++
+		hashMap2[s2[i]-'a']--
+		hashMap2[s2[i+len(s1)]-'a']++
 	}
-	return match(hashMap1, hashMap2)
+	return hashMap1 == hashMap2
 }
