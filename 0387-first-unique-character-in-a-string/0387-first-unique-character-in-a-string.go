8e7afd3cfbdc5717f69fd59995d0d1356cca964a
K.M. Al Jaziz Turja <159911068+aljaziz@users.noreply.github.com>
2026-07-21 17:56:01 +0600

    Time: 0 ms (100%), Space: 8.2 MB (31.24%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.go
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.go
@@ -1,10 +1,10 @@
 func firstUniqChar(s string) int {
-	hashMap := map[byte]int{}
-	for i := 0; i < len(s); i++ {
-		hashMap[s[i]]++
+	hashMap := [26]int{}
+	for _, val := range []byte(s) {
+		hashMap[val-'a']++
 	}
-	for i := 0; i < len(s); i++ {
-		if hashMap[s[i]] == 1 {
+	for i, val := range []byte(s) {
+		if hashMap[val-'a'] == 1 {
 			return i
 		}
 	}
