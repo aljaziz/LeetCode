cc5558f9605f7e8c59bf30e585d6c4a1b37aab72
K.M. Al Jaziz Turja <159911068+aljaziz@users.noreply.github.com>
2026-07-27 17:23:35 +0600

    Time: 1 ms (42.21%), Space: 4.8 MB (49.64%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.go
+++ 0125-valid-palindrome/0125-valid-palindrome.go
@@ -1,20 +1,21 @@
-import "unicode"
-
 func isPalindrome(s string) bool {
-	var newString []rune
-
-	for _, char := range s {
-		if unicode.IsLetter(char) || unicode.IsDigit(char) {
-			newString = append(newString, unicode.ToLower(char))
-		}
-	}
-	l, r := 0, len(newString)-1
+	l, r := 0, len(s)-1
 	for l < r {
-		if newString[l] != newString[r] {
+		for l < r && !isAlphaNumeric(s[l]) {
+			l++
+		}
+		for l < r && !isAlphaNumeric(s[r]) {
+			r--
+		}
+		if strings.ToLower(string(s[l])) != strings.ToLower(string(s[r])) {
 			return false
 		}
 		l++
 		r--
 	}
 	return true
+}
+
+func isAlphaNumeric(char byte) bool {
+	return (char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z') || (char >= '0' && char <= '9')
 }
